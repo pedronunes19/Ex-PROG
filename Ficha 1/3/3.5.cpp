c9b7a3c21efa68d7de0fb8eaf7fc76cfe9e3dcb7
pedronunes19 <pedromiguelmnunes@gmail.com>
2021-03-17 19:59:56 +0000

    updated 3.5

--- Ficha 1/3/3.5.cpp
+++ Ficha 1/3/3.5.cpp
@@ -12,8 +12,26 @@ int gcd(int n0, int n1){
     return n0;
 }
 
+int gcd2(int n0, int n1, int &res){
+    if (n0 == n1) 
+        res = n0;
+    else if (n0 == 0) 
+        res = n1;
+    else if (n1 == 0) 
+        res = n0;
+    else{
+    do{
+        if (n0 < n1) 
+            n1 -= n0;
+        else 
+            n0 -= n1;
+    }while (n0 != n1);
+    res = n0;
+    }
+}
+
 int main() {
-    int a, b;
+    int a, b, res = 0;
     cout << "Input 2 numbers: ";
     cin >> a >> b;
     if (a == 0 && b == 0){
@@ -21,6 +39,7 @@ int main() {
         return 0;
     }
     cout << "GCD = " << gcd(a, b);
+    cout << "GCD = " << gcd2(a, b, res);
     
     return 0;
 }
